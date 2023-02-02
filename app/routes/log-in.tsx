f5e009a16fb42f2b63782abed09aac1655ec9a14
KirdesMF <cedgourville@gmail.com>
2023-02-02 21:45:18 +0100

    add prisma

--- app/routes/log-in.tsx
+++ app/routes/log-in.tsx
@@ -1,3 +1,30 @@
+import { type ActionArgs } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+export async function action({ request }: ActionArgs) {
+  const body = await request.formData();
+  const email = body.get("email") as string;
+  const password = body.get("password") as string;
+}
+
 export default function Login() {
-  return <main>Log in</main>;
+  return (
+    <main>
+      <h1>Log In</h1>
+
+      <Form>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+
+        <button type="submit">Log In</button>
+      </Form>
+    </main>
+  );
 }
